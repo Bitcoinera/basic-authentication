e1e16636ecf2c37376c5b578938a17c312fc041d
anagutjor <anagjordano@gmail.com>
2019-04-02 13:37:06 +0200

    disallow duplicated secrets

--- controllers/secret.js
+++ controllers/secret.js
@@ -2,10 +2,18 @@ const Secret = require('../models/secretModel');
 
 module.exports = {
     async saveSecret(secret) {
-        await Secret.create(secret, err => {
-            if (!err) {
-                console.log('secret successfully submitted');
+        await Secret.findOne(secret, (err, secretFound) => {
+            if (!err && !secretFound) {
+                Secret.create(secret, err => {
+                    if (!err) {
+                        console.log('secret successfully submitted');
+                    }
+                });
+            } else if (err) {
+                console.error(err);
+            } else {
+                console.log('Secret already exists');
             }
-        });
+        })
     }
 }
